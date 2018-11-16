b94c7cd64a5f53654ad533d550fd9392fd2415b9
Alexander Strakhovenko <sasstr@gmail.com>
2018-11-17 00:10:21 +0300

    Исправил ошибки ESLint

--- js/stat.js
+++ js/stat.js
@@ -9,37 +9,37 @@ var CLOUD_HEIGHT = 270;
 var FIRST_LINE_TEXT = 'Ура вы победили!';
 var SECOND_LINE_TEXT = 'Список результатов';
 var FONT = '16px PT Mono';
-var BAR_GRAPH_HEIGHT =  150;
+var BAR_GRAPH_HEIGHT = 150;
 var BAR_GRAPH_WIDTH = 40;
 var BAR_MY_COLOR = 'rgba(255, 0, 0, 1)';
 var BAR_GAP = 50;
 var GAP = 10;
+var TEXT_LINE_X = 140;
+var TEXT_LINE_Y = 40;
 
-var renderStatistics = function (ctx, names, times) {
-// Функция которая возращает лучший результат игры
-var getBestResult = function (times) {
-  return Math.max.apply(null, times);
-};
-var maxResult = getBestResult(times);
+window.renderStatistics = function (ctx, names, times) {
+  // Функция которая возращает лучший результат игры
+  var getBestResult = function () {
+    return Math.max.apply(null, times);
+  };
+  var maxResult = getBestResult(times);
 
-//  Функция возращает массив с высотами гистограмм игроков пропорционально их времени игры
-var playerHeightBar = times.map( function (item, i, times) {
-  return Math.round((item / maxResult) * BAR_GRAPH_HEIGHT);
-});
+  //  Функция возращает массив с высотами гистограмм игроков пропорционально их времени игры
+  var playerHeightBar = times.map(function (item) {
+    return Math.round((item / maxResult) * BAR_GRAPH_HEIGHT);
+  });
 
-// Функция возращает координату Y гистограммы игрока
-var getBarStartCoordinateY = function (heightDifference) {
-  return (100 + heightDifference);
-};
+  // Функция возращает координату Y гистограммы игрока
+  var getBarStartCoordinateY = function (heightDifference) {
+    return (100 + heightDifference);
+  };
 
-// Поучаем цвет гистограмы других игроков или своей.
-var getPlayerBarColor = function (name) {
-  return (name ==='Вы') ? BAR_MY_COLOR : 'hsla(235, 100%, '+ Math.random() * 100 +'% , 1)';
-};
-
-
-    // Функция рисует гистограмы игроков их имена и результаты
-  var drawPlayerBar = function(playerHeightBar) {
+  // Поучаем цвет гистограмы других игроков или своей.
+  var getPlayerBarColor = function (name) {
+    return (name === 'Вы') ? BAR_MY_COLOR : 'hsla(235, 100%, ' + Math.random() * 100 + '% , 1)';
+  };
+  // Функция рисует гистограмы игроков их имена и результаты
+  var drawPlayerBar = function () {
 
     var nextCoordinateX = 155;
     var heightDifference = BAR_GRAPH_HEIGHT;
@@ -52,25 +52,25 @@ var getPlayerBarColor = function (name) {
       ctx.fillRect(nextCoordinateX, nextCoordinateY, BAR_GRAPH_WIDTH, playerHeightBar[i]);
 
       ctx.font = FONT;
-      ctx.textBaseline = "hanging";
-      ctx.strokeText(Math.round(times[i]), nextCoordinateX, (nextCoordinateY-GAP * 2));
+      ctx.textBaseline = 'hanging';
+      ctx.strokeText(Math.round(times[i]), nextCoordinateX, (nextCoordinateY - GAP * 2));
       ctx.strokeText(names[i], nextCoordinateX, 260);
 
-      nextCoordinateX += GAP * 9;
+      nextCoordinateX += BAR_GAP + BAR_GRAPH_WIDTH;
       heightDifference = BAR_GRAPH_HEIGHT;
     }
   };
 
-    ctx.fillStyle = SHADOW_COLOR;
-    ctx.fillRect(CLOUD_COORDINATE_X+GAP, CLOUD_COORDINATE_Y+GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = SHADOW_COLOR;
+  ctx.fillRect(CLOUD_COORDINATE_X + GAP, CLOUD_COORDINATE_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
 
-    ctx.fillStyle = BACKGROUND_COLOR;
-    ctx.fillRect(CLOUD_COORDINATE_X, CLOUD_COORDINATE_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = BACKGROUND_COLOR;
+  ctx.fillRect(CLOUD_COORDINATE_X, CLOUD_COORDINATE_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
-    ctx.font = FONT;
-    ctx.textBaseline = "hanging";
-    ctx.strokeText(FIRST_LINE_TEXT, 140, 40 );
-    ctx.strokeText(SECOND_LINE_TEXT, 140, 60);
+  ctx.font = FONT;
+  ctx.textBaseline = 'hanging';
+  ctx.strokeText(FIRST_LINE_TEXT, TEXT_LINE_X, TEXT_LINE_Y);
+  ctx.strokeText(SECOND_LINE_TEXT, TEXT_LINE_X, TEXT_LINE_Y + 20);
 
-    drawPlayerBar(playerHeightBar);
+  drawPlayerBar(playerHeightBar);
 };
