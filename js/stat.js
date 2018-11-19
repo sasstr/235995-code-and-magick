c4d9f246faa952f0b0510aa9e19bce9895a84996
Alexander Strakhovenko <sasstr@gmail.com>
2018-11-19 12:02:42 +0300

    Внес правки

--- js/stat.js
+++ js/stat.js
@@ -18,6 +18,7 @@ var TEXT_LINE_X = 140;
 var TEXT_LINE_Y = 40;
 var TEXT_SPACE = 100;
 var GRADIENT_COLOR = '#CAEBFE';
+var BASELINE = 'hanging';
 
 window.renderStatistics = function (ctx, names, times) {
   // Функция которая возращает лучший результат игры
@@ -30,6 +31,7 @@ window.renderStatistics = function (ctx, names, times) {
   var getPlayerBarColor = function (name) {
     return (name === 'Вы') ? BAR_MY_COLOR : 'hsla(235, 100%, ' + Math.random() * 100 + '% , 1)';
   };
+
   // Функция рисует гистограмы игроков их имена и результаты
   var drawPlayerBars = function () {
     var nextCoordinateX = 155;
@@ -38,32 +40,25 @@ window.renderStatistics = function (ctx, names, times) {
       var playerHeightBar = Math.round((times[i] / maxResult) * BAR_GRAPH_HEIGHT);
       var nextCoordinateY = TEXT_SPACE + BAR_GRAPH_HEIGHT - playerHeightBar;
 
-      ctx.fillStyle = getPlayerBarColor(names[i]);
-      ctx.fillRect(nextCoordinateX, nextCoordinateY, BAR_GRAPH_WIDTH, playerHeightBar);
-
-      ctx.font = FONT;
-      ctx.textBaseline = 'hanging';
-      ctx.strokeText(Math.round(times[i]), nextCoordinateX, (nextCoordinateY - GAP * 2));
-      ctx.strokeText(names[i], nextCoordinateX, (CLOUD_HEIGHT - GAP));
+      drawBlock(nextCoordinateX, nextCoordinateY, BAR_GRAPH_WIDTH, playerHeightBar, getPlayerBarColor(names[i]));
+      drawText(FONT, BASELINE, Math.round(times[i]), nextCoordinateX, (nextCoordinateY - GAP * 2));
+      drawText(FONT, BASELINE, names[i], nextCoordinateX, (CLOUD_HEIGHT - GAP));
 
       nextCoordinateX += BAR_GAP + BAR_GRAPH_WIDTH;
     }
   };
 
-  var drawCloud = function (x, y, width, height, fillStyle) {
+  // Функция рисует облако
+  var drawBlock = function (x, y, width, height, fillStyle) {
     ctx.fillStyle = fillStyle;
     ctx.fillRect(x, y, width, height);
   };
 
-  var baseLine = 'hanging';
+  // Функция рисует текст
   var drawText = function (font, baseline, text, x, y) {
     ctx.font = font;
     ctx.textBaseline = baseline;
     ctx.strokeText(text, x, y);
-    ctx.shadowColor = 'rgba(0, 0, 0, 0.3)';
-    ctx.shadowBlur = 3;
-    ctx.shadowOffsetX = 3;
-    ctx.shadowOffsetY = 3;
   };
 
   var gradient = ctx.createRadialGradient(238, 50, 10, 238, 50, 300);
@@ -71,9 +66,9 @@ window.renderStatistics = function (ctx, names, times) {
   gradient.addColorStop(1, GRADIENT_COLOR);
   var textSecondLineY = TEXT_LINE_Y + GAP * 2;
 
-  drawCloud(CLOUD_COORDINATE_X + GAP, CLOUD_COORDINATE_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
-  drawCloud(CLOUD_COORDINATE_X, CLOUD_COORDINATE_Y, CLOUD_WIDTH, CLOUD_HEIGHT, gradient);
-  drawText(FONT, baseLine, FIRST_LINE_TEXT, TEXT_LINE_X, TEXT_LINE_Y);
-  drawText(FONT, baseLine, SECOND_LINE_TEXT, TEXT_LINE_X, textSecondLineY);
+  drawBlock(CLOUD_COORDINATE_X + GAP, CLOUD_COORDINATE_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, SHADOW_COLOR);
+  drawBlock(CLOUD_COORDINATE_X, CLOUD_COORDINATE_Y, CLOUD_WIDTH, CLOUD_HEIGHT, gradient);
+  drawText(FONT, BASELINE, FIRST_LINE_TEXT, TEXT_LINE_X, TEXT_LINE_Y);
+  drawText(FONT, BASELINE, SECOND_LINE_TEXT, TEXT_LINE_X, textSecondLineY);
   drawPlayerBars(times);
 };
