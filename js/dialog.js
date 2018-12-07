422ab17eb7f2b9fb70ea53d138fc7c6e951cd5a5
Alexander Strakhovenko <sasstr@gmail.com>
2018-12-07 03:28:38 +0300

    Добился что окно переносится, осталось отписатся от события загрузки файла

--- js/dialog.js
+++ js/dialog.js
@@ -1,5 +1,5 @@
 'use strict';
-/*TODO 
+/* TODO
 
 1/ Диалог должен начинать двигаться за курсором мыши при нажатии (mousedown)  ---
 на блок .setup-user-pic;
@@ -7,57 +7,58 @@
 2/ Диалог должен переставать двигаться за курсором мыши при отпускании    ----
 (mouseup) кнопки мыши и оставаться на новом месте;
 
-3/ При повторном открытии/закрытии диалога, положение диалога должно сбрасываться 
+3/ При повторном открытии/закрытии диалога, положение диалога должно сбрасываться
 на изначальное; ----
 */
 (function () {
-var setupDialogElement = document.querySelector('.setup');
-var dialogHandler = setupDialogElement.querySelector('.setup-user-pic');
+  var setupDialogElement = document.querySelector('.setup');
+  var dialogHandler = setupDialogElement.querySelector('.setup-user-pic');
+  var upload = setupDialogElement.querySelector('.upload');
 
-dialogHandler.addEventListener('mousedown', function (evt) {
+  upload.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
-
+    console.log('Hello world!');
     var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
+      x: evt.clientX,
+      y: evt.clientY
     };
 
     var dragged = false;
 
     var MouseMoveHandler = function (moveEvt) {
-    moveEvt.preventDefault();
-    dragged = true;
-
-    var shift = {
+      moveEvt.preventDefault();
+      dragged = true;
+      console.log('Hello');
+      var shift = {
         x: startCoords.x - moveEvt.clientX,
         y: startCoords.y - moveEvt.clientY
-    };
+      };
 
-    startCoords = {
+      startCoords = {
         x: moveEvt.clientX,
         y: moveEvt.clientY
-    };
+      };
 
-    setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
-    setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+      setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
+      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
     };
 
     var MouseUpHandler = function (upEvt) {
-    upEvt.preventDefault();
+      upEvt.preventDefault();
 
-    document.removeEventListener('mousemove', MouseMoveHandler);
-    document.removeEventListener('mouseup', MouseUpHandler);
+      document.removeEventListener('mousemove', MouseMoveHandler, true);
+      document.removeEventListener('mouseup', MouseUpHandler, true);
 
-    if (dragged) {
-        var сlickPreventDefaultHandler = function (evt) {
-        evt.preventDefault();
-        dialogHandler.removeEventListener('click', сlickPreventDefaultHandler);
+      if (dragged) {
+        var сlickPreventDefaultHandler = function (evtPrevent) {
+          evtPrevent.preventDefault();
+          dialogHandler.removeEventListener('click', сlickPreventDefaultHandler, true);
         };
-        dialogHandler.addEventListener('click', сlickPreventDefaultHandler);
-    }
-  };
+        dialogHandler.addEventListener('click', сlickPreventDefaultHandler, true);
+      }
+    };
 
-    document.addEventListener('mousemove', MouseMoveHandler);
-    document.addEventListener('mouseup', MouseUpHandler);
-});
+    document.addEventListener('mousemove', MouseMoveHandler, true);
+    document.addEventListener('mouseup', MouseUpHandler, true);
+  });
 })();
